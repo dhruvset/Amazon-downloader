589179176a1d3fedc662526aa506fa6d6ebd2ac6
Dhruv Seth <dhruvset@buffalo.edu>
2016-10-14 15:27:17 -0700

    using curl instead

--- review.pl
+++ review.pl
@@ -1,21 +1,5 @@
 #!/usr/bin/perl
 
-# Amazon reviews downloader
-# Copyright (C) 2015  Andrea Esuli
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 use strict;
 use LWP::UserAgent; 
 use HTTP::Request;
@@ -42,11 +26,6 @@ if (length $domain == 0) {
 
 print "\nAmazon ID = $amazon_id, First Page = $f_page, Last Page = $l_page, domain = $domain\n";
 
-my $ua = LWP::UserAgent->new;
-$ua->timeout(10);
-$ua->env_proxy;
-#$ua->agent('Mozilla/5.0 (X11; Linux i686) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.91 Chrome/12.0.742.91 Safari/534.30');
-
 mkdir "$temp_dir";
 mkdir "$temp_dir/$domain";
 
@@ -57,7 +36,7 @@ if ($amazon_id) {
     my $dir = "$temp_dir/$domain/$amazon_id";
     mkdir $dir;
 
-    my $urlPart1 = "http://www.amazon.".$domain."/product-reviews/";
+    my $urlPart1 = "https://www.amazon.".$domain."/product-reviews/";
     my $urlPart2 = "/?ie=UTF8&showViewpoints=0&pageNumber=";
     my $urlPart3 = "&sortBy=bySubmissionDateDescending";
 
@@ -69,50 +48,10 @@ if ($amazon_id) {
     while($page<=$lastPage) {
 
 		my $url = $urlPart1.$amazon_id.$urlPart2.$page.$urlPart3;
-		###print $url;
-		my $request = HTTP::Request->new(GET => $url);
-		$request->referer($referer);
-
-		my $response = $ua->request($request);
-		if($response->is_success) {
-			print " GOTIT\n";
-			my $content = $response->decoded_content;
-
-			while($content =~ m#cm_cr_arp_d_paging_btm_([0-9]+)#gs ) {
-				my $val = $1+0;
-				if($val>$lastPage) {
-					$lastPage = $val;
-				}
-			}
-    $lastPage = $l_page;
-			
-			if(open(CONTENTFILE, ">./$dir/$page")) {
-				binmode(CONTENTFILE, ":utf8");
-				print CONTENTFILE $content;
-				close(CONTENTFILE);
-				print "ok\t$domain\t$amazon_id\t$page\t$lastPage\n";
-			}
-			else {
-				print "failed\t$domain\t$amazon_id\t$page\t$lastPage\n";
-			}
-			
-			if($sleepTime>0) {
-			#	--$sleepTime;
-			}
-		}
-		else {
-			if($response->code==503) {
-				--$page;
-				#++$sleepTime;
-				print " TIMEOUT ".$response->code." retrying (new timeout $sleepTime)\n";
-			}
-			else {
-				print " Downloaded ". ($page-1). " pages for product id $amazon_id (end code:".$response->code.")\n";
-				last;
-			}
-		}
+                my $cmd = "curl -s -o $dir/$page \"$url\"";
+                #print $cmd;
+                system($cmd);
 		++$page;
-		sleep($sleepTime);
     }
 }
 system("touch $temp_dir/$thread_id");
